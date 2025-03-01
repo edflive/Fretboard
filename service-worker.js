5898bccd8f99c004072f42b34f99cf124e9f6260
edflive <edouard.feuillet@live.fr>
2025-03-01 21:04:25 +0100

    Fretboard V1.1.1

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,14 @@
-const CACHE_NAME = "fretboard-cache-v1.1";
+const CACHE_NAME = "fretboard-cache-v1.1.1";
 const urlsToCache = [
-  "./",                // racine
-  "./index.html",      // page principale
-  "./css/style.css",   // CSS
-  "./js/script.js",    // JS
-  "./images/fretboard13.svg",
-  "./images/fretboard16.svg",
-  "./images/fretboard20.svg",
-  "./images/icon-192.png",
-  "./images/icon-512.png"
+  "/",                // racine
+  "/index.html",      // page principale
+  "/css/style.css",   // CSS
+  "/js/script.js",    // JS
+  "/images/fretboard13.svg",
+  "/images/fretboard16.svg",
+  "/images/fretboard20.svg",
+  "/images/icon-192.png",
+  "/images/icon-512.png"
 ];
 
 // Installation du service worker
@@ -39,8 +39,12 @@ self.addEventListener("activate", event => {
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
-      // Retourne la réponse du cache si disponible, sinon fetch réseau
-      return response || fetch(event.request);
-    })
+      return response || fetch(event.request).then(networkResponse => {
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+      });
+    }).catch(() => caches.match("/index.html")) // Retourne index.html si tout échoue (PWA comportement classique)
   );
 });
